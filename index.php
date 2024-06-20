09f1f19778ec8f1be6a6397c7dd452076b823f5f
Thomas Blachowiak <blachowiak.thomas@outlook.fr>
2024-06-20 21:53:30 +0200

    create file styles.css, modify home page

--- index.php
+++ index.php
@@ -3,7 +3,18 @@ require_once "includes/header.php";
 include_once "includes/nav.php";
 ?>
 
-<h1 class="text-center">Page d'accueil</h1>
+<div class="d-flex align-items-center justify-content-center" id="about-me">
+    <div class="container">
+        <div class="col">
+            <h1 class="p-4 fs-1 ">
+                <strong>Les Rabelaisiens</strong>
+            </h1>
+            <p class="p-2 fs-3">
+                <strong>Le site qui va vous faire saliver, <br> partager vos meilleurs recettes !!! </strong>
+            </p>
+        </div>
+    </div>
+</div>
 
 
 <?php
